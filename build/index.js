0b88d071ca0289d42d705ca635750f4f5c6ae91e
marquisf <marquisfffff@gmail.com>
2018-12-20 18:36:53 +0800

    Changed package.json scripts for Unix

---
+++ build/index.js
@@ -0,0 +1,23 @@
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
+var createSquare = function (config) {
+    var newSquare = { color: 'White', area: 0 };
+    if (config.color) {
+        newSquare.color = config.color;
+    }
+    if (config.width) {
+        newSquare.area = config.width * config.width;
+    }
+    return __assign({}, config, newSquare);
+};
+var square_1 = createSquare({ color: 'black', width: 0, owner: 'Eric' });
+console.log(square_1);
